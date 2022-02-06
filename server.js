4336c342e2bf57ed895f6b8a862603d4d31681ab
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-06 23:01:13 +0100

    configuring basic routes

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
+// ROUTES
+
+//get all restaurantes
+app.get("/api/restaurants", (req, res) => {
+  res.json("all restaurantes");
+});
+
+//get single restaurante
+app.get("/api/restaurants/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({ msg: "single restaurante", id });
+});
+
+//create restaurante
+app.get("/api/restaurants", (req, res) => {
+  const data = req.body;
+  res.status(200).json({ msg: "created new restaurantes", data });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serverr is operational on port: ${port}`);
