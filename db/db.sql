a4db2b643aa23e374bc1face150bf4c604c2bf20
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-10 19:55:00 +0100

    added reviews table

--- db/db.sql
+++ db/db.sql
@@ -13,8 +13,19 @@ CREATE TABLE restaurants (
 );
 
 
+
+
 INSERT INTO restaurants ( name, location, price_range) values ( 'mcDonalds', 'Sarajevo', 3);
 INSERT INTO restaurants ( name, location, price_range) values ( 'mahir', 'Sarajevo', 2);
 INSERT INTO restaurants ( name, location, price_range) values ( 'zeljo', 'Sarajevo', 5);
 INSERT INTO restaurants ( name, location, price_range) values ( 'mcDonalds', 'Mostar', 3);
 
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id  BIGINT NOT NULL REFERENCES  restaurants(id),
+    name VARCHAR(50),
+    review text NOT NULL,
+    rating INT NOT NULL check (rating >=1 and rating <=5)
+);
+
+INSERT INTO reviews (restaurant_id, name, review, rating)  values (1, 'buco', 'ovo je moj test', 3);
